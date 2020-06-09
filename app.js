b41fb3b1a52ad6f5f8fcd0af8a474353bbcb7421
SpyrosGeo <xarodasimba@gmail.com>
2020-06-09 20:53:39 +0300

    added filtering the prominent expression and adding it to an h1 for now. next i want to play specific audio tracks depending on the expression/mood

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const video = document.querySelector('#video');
+const prediction = document.querySelector('#h1')
 
 //loading the models
 Promise.all([
@@ -12,27 +13,37 @@ Promise.all([
 
 function startVideo() {
     navigator.mediaDevices.getUserMedia(
-        {video: {}})
-        .then(stream=>{
+        { video: {} })
+        .then(stream => {
             video.srcObject = stream
         })
-        .catch(err=>{
+        .catch(err => {
             console.log(err)
-        })           
+        })
 }
 // startVideo()
-video.addEventListener('playing',()=>{
+video.addEventListener('playing', () => {
     const canvas = faceapi.createCanvasFromMedia(video)
     document.body.append(canvas)
-    const displaySize = {width:video.width,height:video.height}
-    faceapi.matchDimensions(canvas,displaySize)
-    setInterval(async()=>{
-        const detections = await faceapi.detectAllFaces(video,new faceapi.TinyFaceDetectorOptions())
+    const displaySize = { width: video.width, height: video.height }
+    faceapi.matchDimensions(canvas, displaySize)
+    setInterval(async () => {
+        const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
             .withFaceLandmarks()
             .withFaceExpressions()
 
-            const resizedDetections = faceapi.resizeResults(detections,displaySize)
-            canvas.getContext('2d').clearRect(0,0,canvas.width,canvas.height)
-            faceapi.draw.drawDetections(canvas,resizedDetections)
-    },1000)
+        // const resizedDetections = faceapi.resizeResults(detections, displaySize)
+        // canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+        // faceapi.draw.drawDetections(canvas, resizedDetections)
+        // faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+        // faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+
+        if (detections[0]) {
+            const { expressions } = detections[0]
+            // console.log(expressions)
+            result = Object.keys(expressions).reduce((a, b) =>
+                expressions[a] > expressions[b] ? a : b)
+                prediction.innerHTML = result
+        }
+    }, 1000)
 })
